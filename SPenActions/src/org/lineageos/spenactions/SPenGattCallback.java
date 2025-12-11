2c7abc144ee372d79ece40976be2450c48339f18
Bruno Martins <bgcngm@gmail.com>
2025-12-11 00:15:15 +0000

    exynos9820-common: SPenActions: Use proper API to obtain InputManager instance
    The usage of InputManager.getInstance() is not supported anymore
    and the method is even getting removed in BP4A.
    
    Change-Id: If5460049296ed354cae6ec27843ffab57d781905

--- SPenActions/src/org/lineageos/spenactions/SPenGattCallback.java
+++ SPenActions/src/org/lineageos/spenactions/SPenGattCallback.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 The LineageOS Project
+ * Copyright (c) 2021-2025 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -182,7 +182,7 @@ public class SPenGattCallback extends BluetoothGattCallback {
                 InputDevice.SOURCE_KEYBOARD);
 
         ev.setDisplayId(mContext.getDisplay().getDisplayId());
-        return InputManager.getInstance()
+        return mContext.getSystemService(InputManager.class)
                 .injectInputEvent(ev, InputManager.INJECT_INPUT_EVENT_MODE_ASYNC);
     }
 
