34e01cfdd83add7caa95d6d1d450b8bc8d27164a
ziomek3120 <unboxorg@gmail.com>
2025-12-30 23:37:45 +0100

    Revert "exynos9820-common: Switch to AIDL NFC HAL"
    This reverts commit dc4aa47792f417c1bc05b3c01121d706a4e161c0.

--- common.mk
+++ common.mk
@@ -165,7 +165,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.sec \
+    android.hardware.nfc@1.2-service.samsung \
     com.android.nfc_extras \
     Tag
 
